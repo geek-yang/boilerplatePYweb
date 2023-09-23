ee2100809adbeb39d05ea134c8df1c5e1b43634f
geek-yang <liuyang.bach@gmail.com>
2023-09-23 22:54:20 +0200

    add delete and handle exceptions

--- pyweb/main.py
+++ pyweb/main.py
@@ -1,6 +1,6 @@
 """Main function of our RESTful Flask app."""
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 
 app = Flask(__name__)
@@ -20,14 +20,31 @@ video_put_args.add_argument(
 videos = {}
 
 
+def if_video_id_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid.")
+
+
+def if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(409, message="Video id already exists.")
+
+
 class Video(Resource):
     def get(self, video_id):
+        if_video_id_not_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        if_video_id_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        if_video_id_not_exist(video_id)
+        del videos[video_id]
+        return "", 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
