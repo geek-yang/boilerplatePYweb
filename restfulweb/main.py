3c5d35f798431d7c33015e0c8882f6e89c3112c1
geek-yang <liuyang.bach@gmail.com>
2023-09-30 12:38:06 +0200

    restructure restful web app

--- restfulweb/main.py
+++ restfulweb/main.py
@@ -1,5 +1,5 @@
 """RESTful Flask app with SQLAlchemy."""
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
@@ -43,12 +43,9 @@ video_put_args.add_argument(
 
 # Parser for patch
 video_update_args = reqparse.RequestParser()
-video_update_args.add_argument(
-    "name", type=str, help="Name of the video is required")
-video_update_args.add_argument(
-    "views", type=int, help="Views of the video is required")
-video_update_args.add_argument(
-    "likes", type=int, help="Likes on the video is required")
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video is required")
+video_update_args.add_argument("likes", type=int, help="Likes on the video is required")
 
 resource_fields = {
     "id": fields.Integer,
@@ -73,7 +70,6 @@ class Video(Resource):
         # check duplication
         if result:
             abort(409, message="Video ID is already taken!")
-
         video = VideoModel(
             id=video_id, name=args["name"], views=args["views"], likes=args["likes"]
         )
@@ -87,18 +83,17 @@ class Video(Resource):
         result = VideoModel.query.filter_by(id=video_id).first()
         if not result:
             abort(404, message="Video does not exist and can not be updated!")
-        
+
         if args["name"]:
             result.name = args["name"]
         if args["views"]:
             result.views = args["views"]
         if args["likes"]:
             result.likes = args["likes"]
-        
+
         db.session.commit()
 
         return result
-
 
     def delete(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
@@ -109,7 +104,12 @@ class Video(Resource):
         return "", 204
 
 
-api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(Video, "/metadata")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", videos = VideoModel.query.all())
 
 
 if __name__ == "__main__":
